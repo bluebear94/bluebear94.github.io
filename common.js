a0258a27a07312cf785dff16ce247381d51f1bda
bluebear94 <tkook11@gmail.com>
2017-03-17 00:06:20 -0400

    blah

---
+++ common.js
@@ -0,0 +1,11 @@
+const message = `%c
+<shameless-self-promotion>
+Hey! Love to code?
+Come join the AGC Discord below!
+             ↓↓
+→ https://discord.gg/Mq7m4 ←
+             ↑↑
+</shameless-self-promotion>
+`;
+
+console.info(message, 'font-family: "Liberation Mono", "DejaVu Sans Mono", monospace; color: #AA2000;');
