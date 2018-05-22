be8d8ce2011fecf6c6a059333d3f43123ae686dd
bluebear94 <uruwi@protonmail.com>
2018-05-22 08:47:28 -0400

    Add options page

--- common.js
+++ common.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const message = `%c
 <shameless-self-promotion>
 Hey! Love to code?
@@ -20,6 +22,13 @@ const hacmboxHTML = `
 
 console.info(message, 'font-family: "Liberation Mono", "DejaVu Sans Mono", monospace; color: #AA2000;');
 
+var hacmToggleRule;
+var hacmMenuToggle;
+var hacmbox;
+var hacmNameBox;
+var data = window.localStorage;
+var optpage = location.pathname.endsWith("options.html");
+
 function getCSSRule(selector) {
   for(var i = 0; i < document.styleSheets.length; ++i) {
     var sheet = document.styleSheets[i];
@@ -32,29 +41,78 @@ function getCSSRule(selector) {
   return undefined;
 }
 
-var hacmToggleRule;
-
-window.onload = function() {
-  var body = document.getElementsByTagName("body")[0];
-  var hacmbox = document.createElement("div");
-  hacmbox.setAttribute("class", "hacmtoggle");
-  hacmbox.innerHTML = hacmboxHTML;
-  body.insertBefore(hacmbox, body.firstChild);
-  hacmToggleRule = getCSSRule("hacm, .hacm");
-  if (hacmToggleRule === undefined) {
-    console.error("Hacm toggle rule not found.");
-  }
-  console.log("hacmbox done");
-};
-
-function toggleHacm(alwaysInvert) {
-  var hacmbox = document.getElementById("hacmtoggle");
-  var useHacm = hacmbox.checked;
-  if (alwaysInvert) useHacm = !useHacm;
+function setHacm(useHacm) {
   if (useHacm) {
-    hacmToggleRule.style.setProperty("font-family", '"kardinal", sans-serif');
+    var fontName = data.getItem("hacmFont") || "kardinal";
+    hacmToggleRule.style.setProperty(
+      "font-family",
+      '"' + fontName + '", sans-serif');
   } else {
     hacmToggleRule.style.setProperty("font-family", 'sans-serif');
   }
   hacmbox.checked = useHacm;
+  data.setItem("hacm", useHacm ? "on" : "off");
+}
+
+function toggleHacm(alwaysInvert) {
+  var useHacm = hacmbox.checked;
+  if (alwaysInvert) useHacm = !useHacm;
+  setHacm(useHacm);
+}
+
+function setHacmMenu(show) {
+  data.setItem("hacmMenu", show ? "on" : "off");
+  if (optpage) hacmMenuToggle.checked = show;
+}
+
+function toggleHacmMenu() {
+  var show = hacmMenuToggle.checked;
+  setHacmMenu(show);
+}
+
+function setHacmFont(name) {
+  data.setItem("hacmFont", name);
+  if (optpage) hacmNameBox.value = name;
+  toggleHacm(false);
+}
+
+function toggleHacmFont() {
+  setHacmFont(hacmNameBox.value);
+}
+
+function windowOnLoadCommon() {
+  hacmToggleRule = getCSSRule("hacm, .hacm");
+  if (hacmToggleRule === undefined) {
+    console.error("Hacm toggle rule not found.");
+  }
+  setHacm(data.getItem("hacm") != "off");
+  setHacmMenu(data.getItem("hacmMenu") != "off");
+  setHacmFont(data.getItem("hacmFont") || "kardinal");
+}
+
+if (optpage) {
+  window.onload = function() {
+    hacmbox = document.getElementById("hacmtoggle");
+    hacmMenuToggle = document.getElementById("hacmshow");
+    hacmNameBox = document.getElementById("hacmfont");
+    windowOnLoadCommon();
+  };
+} else {
+  window.onload = function() {
+    if (data.getItem("hacmMenu") == "off") return;
+    var body = document.getElementsByTagName("body")[0];
+    var hacmmenu = document.createElement("div");
+    hacmmenu.setAttribute("class", "hacmtoggle");
+    hacmmenu.innerHTML = hacmboxHTML;
+    body.insertBefore(hacmmenu, body.firstChild);
+    hacmbox = document.getElementById("hacmtoggle");
+    windowOnLoadCommon();
+    if (document.title === "404") {
+      var page = location.pathname;
+      var inserts = document.getElementsByClassName("namehere");
+      for (var i = 0; i < inserts.length; ++i) {
+        inserts[i].textContent = page;
+      }
+    }
+  };
 }
